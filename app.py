81b7c24e7e29596433b755b98eddc3b934097988
Luca Tassinari <luca.tassinari.2000@gmail.com>
2022-07-17 11:22:47 +0200

    added usage

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ import sys
 
 from core.server import WebServerAgent
 
+"""
+Usage:
+    python3 ./app.py [port_number]
+
+Options:
+    port_number     The port number on which http server is listening [default: 8080].
+"""
+
+_DEFAULT_PORT = 8080
+
 if sys.argv[1:]:
     port = int(sys.argv[1])
 else:
-    port = 8080
+    port = _DEFAULT_PORT
 
 server_agent = WebServerAgent()
 server_agent.run(port)
